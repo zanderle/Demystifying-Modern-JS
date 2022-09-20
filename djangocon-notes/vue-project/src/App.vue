6ad5bcd8ed8c2f72d1a66844a244e0ed4839c134
Žan Anderle <zan.anderle@gmail.com>
2022-09-21 00:40:31 +0100

    Create vue project using create-vue and convert our app

---
+++ djangocon-notes/vue-project/src/App.vue
@@ -0,0 +1,68 @@
+<script>
+import schedule from "./assets/schedule";
+
+export default {
+  data() {
+    return {
+      selectedTalk: null,
+      notes: {},
+    };
+  },
+  methods: {
+    showDetails(talk) {
+      this.selectedTalk = talk;
+    },
+  },
+  computed: {
+    talks() {
+      const talks = this.auditorium_talks.map((t) => {
+        t.speakers = t.persons.map((p) => p.public_name).join(", ");
+        return t;
+      });
+      return talks;
+    },
+    auditorium_talks() {
+      const { Auditorium: auditorium_talks } =
+        schedule.schedule.conference.days[0].rooms;
+      return auditorium_talks;
+    },
+  },
+};
+</script>
+
+<template>
+  <ul class="flex-1 h-full overflow-y-scroll">
+    <template v-for="talk in talks">
+      <li v-if="talk.persons.length" :key="talk.id">
+        <h2>{{ talk.title }}</h2>
+        <h4>{{ talk.speakers }}</h4>
+        <p>{{ talk.abstract }}</p>
+        <button @click="showDetails(talk)" class="px-2 py-1 rounded border">
+          Show Details
+        </button>
+        <a :href="talk.url" class="text-blue-400 hover:text-blue-500 mx-4"
+          >See talk on pretalx</a
+        >
+      </li>
+    </template>
+  </ul>
+  <div class="flex-1 h-full overflow-y-scroll px-4">
+    <span class="font-bold"
+      >Notes taken: {{ Object.keys(notes).length }} / {{ talks.length }}</span
+    >
+    <template v-if="selectedTalk">
+      <h2>{{ selectedTalk.title }}</h2>
+      <h4>{{ selectedTalk.speakers }}</h4>
+      <p>{{ selectedTalk.description }}</p>
+      <textarea
+        :name="`notes-{selectedTalk.id}`"
+        :id="`notes-{selectedTalk.id}`"
+        rows="10"
+        class="w-full"
+        v-model="notes[selectedTalk.id]"
+      ></textarea>
+    </template>
+  </div>
+</template>
+
+<style></style>
